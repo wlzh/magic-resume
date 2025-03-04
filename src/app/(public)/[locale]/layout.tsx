d381ff1d71ef266f0d73dbfe4dc98bcf5d34e2ed
wlzh <welizhi@gmail.com>
2025-03-04 14:09:42 +0800

    修改基础信息

--- src/app/(public)/[locale]/layout.tsx
+++ src/app/(public)/[locale]/layout.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata({
   params: { locale }
 }: Props): Promise<Metadata> {
   const t = await getTranslations({ locale, namespace: "common" });
-  const baseUrl = "https://magicv.art";
+  const baseUrl = "https://jianli.869hr.uk";
 
   return {
     title: t("title") + " - " + t("subtitle"),
